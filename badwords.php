d9dee2dea846474d171c5cfff1966a612c190aa0
DrKillex <lcollaro1994@gmail.com>
2023-04-04 15:14:32 +0200

    first commit

---
+++ badwords.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>il testo originale è: '<?php echo $_GET['text']?>' ed è lungo <?php echo strlen($_GET['text'])?> caratteri</p>
+    <p>il testo censurato è: '<?php echo str_replace($_GET['censura'], "***", $_GET['text'])?>' ed è lungo <?php echo strlen(str_replace($_GET['censura'], "***", $_GET['text']))?> caratteri</p>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="badwords.php" method="GET">
+        <label for="text">scrivi testo</label>
+        <input type="text" name='text' id='text'>
+        <label for="censura">parola da censurare</label>
+        <input type="text" name='censura' id='censura'>
+        <button type="submit">submit</button>
+    </form>
+</body>
+</html>
