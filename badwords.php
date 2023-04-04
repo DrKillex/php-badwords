56a66e09f1c061823c9ab60bcce5b34ece8ad18f
DrKillex <lcollaro1994@gmail.com>
2023-04-04 15:19:15 +0200

    modifica keysense

--- badwords.php
+++ badwords.php
@@ -8,6 +8,6 @@
 </head>
 <body>
     <p>il testo originale è: '<?php echo $_GET['text']?>' ed è lungo <?php echo strlen($_GET['text'])?> caratteri</p>
-    <p>il testo censurato è: '<?php echo str_replace($_GET['censura'], "***", $_GET['text'])?>' ed è lungo <?php echo strlen(str_replace($_GET['censura'], "***", $_GET['text']))?> caratteri</p>
+    <p>il testo censurato è: '<?php echo str_ireplace($_GET['censura'], "***", $_GET['text'])?>' ed è lungo <?php echo strlen(str_ireplace($_GET['censura'], "***", $_GET['text']))?> caratteri</p>
 </body>
 </html>
